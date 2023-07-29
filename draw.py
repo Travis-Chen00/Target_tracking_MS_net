adcff1b481be3d2d848a3db735737f42c1af8210
Boyu Chen <boyuchen00@hotmail.com>
2023-07-30 03:17:49 +0800

    Dynamic drawing

--- draw.py
+++ draw.py
@@ -5,48 +5,32 @@ from utils.util import Agent, Pos
 from parameters.STD14 import *
 import numpy as np
 import math
-
+import time
 
 def random_location(final, target, sizeX, sizeY, move):
-    # 计算 N 的值
     num_targets = len(target)
     N = math.ceil(math.sqrt(num_targets))  # 向上取整
-
-    # 创建 NxN 子图
-    fig, axs = plt.subplots(N, N, figsize=(12, 12))
-    fig.subplots_adjust(wspace=0.5, hspace=0.5)
-
-    # 为了方便处理，如果 axs 不是二维数组，那么我们将其转化为二维数组
-    if len(axs.shape) < 2:
-        axs = np.array([[axs]])
-
     prev_x, prev_y = None, None  # 初始化上一次的目标位置
     move_num = 1
 
+    plt.ion()  # 开启交互模式
+    fig = plt.figure()  # 创建新的figure
+
     for ge in range(N):
         for j in range(N):
-            # 计算当前目标的索引
-            idx = ge * N + j
+            idx = ge * N + j  # 计算当前目标的索引
 
-            print(idx)
-
-            # 如果当前索引大于目标的数量，那么就没有其他的子图需要画，所以直接退出
-            if idx >= num_targets:
-                axs[ge][j].axis('off')  # 隐藏没有使用的子图
+            if idx >= num_targets:  # 如果当前索引大于目标的数量，那么就没有其他的子图需要画，所以直接退出
                 continue
 
             x = int(target[idx][0]) + 1  # 小网格中心的x坐标
             y = int(target[idx][1]) + 1  # 小网格中心的y坐标
 
-            # 如果上一次的目标位置已经被定义，并且和当前目标位置不同，那么重置 move_num
-            if prev_x is not None and prev_y is not None and (prev_x != x or prev_y != y):
+            if prev_x is not None and prev_y is not None and (prev_x != x or prev_y != y):  # 如果上一次的目标位置已经被定义，并且和当前目标位置不同，那么重置 move_num
                 move_num = 1
+            prev_x, prev_y = x, y  # 记录当前目标位置，用于下一次循环
 
-            # 记录当前目标位置，用于下一次循环
-            prev_x, prev_y = x, y
-
-            # 创建一个稍大的2D数组，颜色值为蓝色的RGB
-            color_grid = np.full((sizeX + 2, sizeY + 2, 3), [188, 216, 235]) / 255.0  # 浅蓝色
+            color_grid = np.full((sizeX + 2, sizeY + 2, 3), [188, 216, 235]) / 255.0  # 创建一个稍大的2D数组，颜色值为蓝色的RGB
 
             # 将目标周围的一圈（9个格子）设为红色
             for dx in range(-5, 6):
@@ -57,40 +41,34 @@ def random_location(final, target, sizeX, sizeY, move):
                     elif dist < 5:
                         color_grid[y + dy, x + dx] = np.array([255, 165, 100]) / 255.0  # 浅橘色
 
-            # 清除之前的内容
-            axs[ge][j].clear()
+            plt.clf()  # 清除当前的figure
+            ax = fig.add_subplot(111)  # 将当前子图添加到figure中
 
             # 添加新的内容
             if idx == 0:
-                filename = "Initial Figure/" + "Target: " + str(target[idx][0]) + ", " + str(target[idx][1])
-                axs[ge][j].set_title(filename)
+                filename = "Initial " + "Target: " + str(target[idx][0]) + ", " + str(target[idx][1])
+                ax.set_title(filename)
             else:
                 filename = "Target: " + str(target[idx][0]) + ", " + str(target[idx][1]) + " Move " + str(move_num)
                 move_num += 1
-                axs[ge][j].set_title(filename)
+                ax.set_title(filename)
 
             circle = Circle((x, y), 0.28, color='red')
-            axs[ge][j].add_patch(circle)
-            axs[ge][j].imshow(color_grid, origin='lower')
+            ax.add_patch(circle)
+            ax.imshow(color_grid, origin='lower')
 
             # 绘制网格
-            axs[ge][j].plot([1, sizeX, sizeY, 1, 1], [1, 1, sizeX, sizeY, 0], color='black', linewidth=0.4,
+            ax.plot([1, sizeX, sizeY, 1, 1], [1, 1, sizeX, sizeY, 0], color='black', linewidth=0.4,
                             linestyle="dashed")
             for i in range(1, sizeY):
-                axs[ge][j].plot([i, i], [1, sizeY], color='black', linewidth=0.5, linestyle="dashed")
+                ax.plot([i, i], [1, sizeY], color='black', linewidth=0.5, linestyle="dashed")
             for i in range(1, sizeX):
-                axs[ge][j].plot([1, sizeX], [i, i], color='black', linewidth=0.5, linestyle="dashed")
+                ax.plot([1, sizeX], [i, i], color='black', linewidth=0.5, linestyle="dashed")
 
-            axs[ge][j].set_xlim(0, sizeX + 1)
-            axs[ge][j].set_ylim(0, sizeY + 1)
+            ax.set_xlim(0, sizeX + 1)
+            ax.set_ylim(0, sizeY + 1)
 
-    for ge in range(N):
-        for j in range(N):
-            # 计算当前目标的索引
-            idx = ge * N + j
-            if idx >= num_targets:
-                continue
-
+            # 下面的代码块是处理绘制 agents 的逻辑，我们需要将它移到每个子图的绘制部分
             for i in range(NUM_AGENTS):
                 x = int(final[idx][i].coord.x) + 1
                 y = int(final[idx][i].coord.y) + 1
@@ -105,8 +83,12 @@ def random_location(final, target, sizeX, sizeY, move):
                     vertices = [(x - 0.28, y + 0.28), (x + 0.28, y + 0.28), (x, y - 0.28)]
 
                 triangle = Polygon(vertices, closed=True, edgecolor='black', facecolor='black')
-                axs[ge][j].add_patch(triangle)
+                ax.add_patch(triangle)
 
+            plt.draw()  # 绘制图像
+            plt.pause(1.5)  # 等待0.5秒
+
+    plt.ioff()  # 关闭交互模式
     plt.show()
 
 
