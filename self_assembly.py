963545679306c0e63f90b13485b33a66d3bdc073
Boyu Chen <boyuchen00@hotmail.com>
2023-08-16 20:41:13 +0800

    Update images

--- self_assembly.py
+++ self_assembly.py
@@ -1,4 +1,3 @@
-import copy
 import os
 import numpy as np
 import random
@@ -6,7 +5,6 @@ from utils.util import Pos, Agent
 from parameters.STD14 import *
 from minimal_surprise import MinimalSurprise
 from utils.sensors import *
-from draw import random_location
 
 
 class SelfAssembly:
